faf34b8e3b9e2167ff9f876083a6c389c2ae9b36
xkevinpham <57520301+xkevinpham@users.noreply.github.com>
2020-04-14 16:17:02 -0700

    update script.js

--- public/script.js
+++ public/script.js
@@ -23,19 +23,19 @@ searchBox.addListener('places_changed', () => {
     })
 })
 
-const locationElement = document.querySelector('[data-location]')
-const statusElement = document.querySelector('[data-status]')
-const temperatureElement = document.querySelector('[data-temperature]')
-const precipitationElement = document.querySelector('[data-precipitationElement]')
-const windElement = document.querySelector('[data-wind]')
+const location = document.querySelector('[data-location]')
+const status = document.querySelector('[data-status]')
+const temperature = document.querySelector('[data-temperature]')
+const precipitation = document.querySelector('[data-precipitationElement]')
+const windSpeed = document.querySelector('[data-wind]')
 
 function setWeatherData(data, place) {
     //console.log(place)
     //console.log(data.weather.description)
-    locationElement.textContent = place
-    statusElement.textContent = data.weather.description
-    temperatureElement.textContent = data.app_temp
-    windElement.textContent = data.wind_spd
-    precipitationElement.textContent = data.precip
+    location.textContent = place
+    status.textContent = data.weather.description
+    temperature.textContent = data.app_temp
+    windSpeed.textContent = data.wind_spd
+    precipitation.textContent = data.precip
     
 }
